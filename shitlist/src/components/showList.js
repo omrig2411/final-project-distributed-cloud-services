3fde5f84a03c3986d247e03e8b183a104effa228
omrig2411 <37674064+omrig2411@users.noreply.github.com>
2019-02-20 22:30:52 +0200

    commit

--- shitlist/src/components/showList.js
+++ shitlist/src/components/showList.js
@@ -1,5 +1,6 @@
 import React, {Component } from 'react'
 import Show from './show'
+// import {List} from 'react-virtualized'
 
 class showList extends Component {
     constructor(props) {
@@ -15,21 +16,23 @@ class showList extends Component {
     eachshow(show, i) {
         return (
             <div key={`container${i}`} className="card" style={{width: 18 + 'rem', marginBottom: 7 + 'px'}}>
+                <img class="card-img-top" src= {show.img_url} alt="card image cap"></img>
                 <div className="card-body">
                     <Show key={`show${i}`} index={i}>
-                        <h3>{show.name}</h3>
+                        <h3 class="card-title">{show.name}</h3>
                         <h5>Start Date: {show.startDate}</h5>
                         <h6>Rating: {show.avgRating}</h6>
                         <h6>Rated by: {show.numOfUserRated} users.</h6>
                         <h6>Status: {show.status}</h6>
-                        <img src= {show.img_url}></img>
+                        <a href="#" class="btn btn-primary">Rate Show</a>
+                        
                     </Show>
                 </div>
             </div>
         )
     }
 
-    add({ id = null, name = 'default name', start_date = 'default points', average_rating = 'default', num_of_users_rating = 'default', status = 'default', img_url = 'default'}) {
+    add({ id = null, name = 'default name', start_date = 'unknown', average_rating = 'not rated yet', num_of_users_rating = '0', status = 'default', img_url = 'default'}) {
         this.setState(prevState => ({
           shows: [
             ...prevState.shows, {
@@ -53,7 +56,7 @@ class showList extends Component {
         const url = 'https://shit-list-zahor-omri.herokuapp.com/show/getallshows';
         fetch(url)
             .then(res => res.json())
-            .then(data => data.map(item =>
+            .then(data => data.slice(0,100).map(item =>
                 this.add({name: item.name,
                         startDate: item.startDate,
                         avgRating: item.avgRating,
